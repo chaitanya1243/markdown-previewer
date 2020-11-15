71750f9e39416f4ce45070e62212a237fe60c8ce
chaitanya <sainagachaitanya.poranki@gmail.com>
2020-11-15 18:12:59 +0530

    all added, required for a functional md previewer

--- index.js
+++ index.js
@@ -1,4 +1,56 @@
 const ROOT = document.getElementById('root')
+const DEFAULT_MD = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+// function crtobr(text){
+//     var count = 0
+//     for(let i of text){
+//         if (i === '\n')
+//         count++
+//     }
+//     return count
+// }
 
 class Editor extends React.Component{
     constructor(props){
@@ -24,18 +76,21 @@ class App extends React.Component{
     constructor(props){
         super(props)
         this.state = {
-            inputValue: `# The Markdown Previewer
-            ## created using marked api
-            `
+            inputValue: ''
         }
         this.handleChange = this.handleChange.bind(this)
         this.renderMarkup = this.renderMarkup.bind(this)
+    }
+    componentDidMount(){
+        this.setState({
+            inputValue: DEFAULT_MD
+        })
     }
     handleChange(event){
         this.setState({
             inputValue: event.target.value
         })
-        console.log(this.renderMarkup())
+        // console.log(this.renderMarkup())
     }
     renderMarkup(){
         marked.setOptions({
@@ -56,11 +111,12 @@ class App extends React.Component{
     }
     render(){
         return (
-        <div>
+        <div className='wrapper'>
         <Editor inputValue={this.state.inputValue} handleChange={this.handleChange}/>
         <Preview markedContent={this.renderMarkup()}/>
         </div>
         )
     }
 }
+
 ReactDOM.render(<App />, ROOT)
